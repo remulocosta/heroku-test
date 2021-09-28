bde2efcf075a4bba1b76184827a4b54e506a464a
gneto <glaudistonson@gmail.com>
2021-09-27 23:36:45 -0300

    trying again

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,14 @@
-const {db} = require('./.env');
+require('dotenv').config();
 
 module.exports = {
   client: 'mysql2',
-  connection: db,
+  connection: ({
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER
+  }),
   pool: {
     min: 2,
     max: 10
