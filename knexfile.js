d87d8a87c0c36c05ac754ef281c13c1ac7ca58ba
gneto <glaudistonson@gmail.com>
2021-09-28 01:06:46 -0300

    it never works

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,8 @@
-require('dotenv').config();
+const { db } = require('./.env')
 
 module.exports = {
   client: 'mysql2',
-  connection: ({
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER
-  }),
+  connection: db,
   pool: {
     min: 2,
     max: 10
